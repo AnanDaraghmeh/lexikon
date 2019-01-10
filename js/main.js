1aa7b938c0a8d7e6a4087fc2601d6c199f548231
AnanDaraghmeh <anan.daraghmeh@yahoo.com>
2019-01-10 08:27:31 +0100

    add swedish definitions

--- js/main.js
+++ js/main.js
@@ -2,21 +2,29 @@ $(function(){
 
 $('#searchField').on('keypress', (e)=>{
         if (e.which === 13){
-        let userInput = e.target.value;
         e.preventDefault();
+        let userInput = trimUserinput(e.target.value);
+        console.log(e.target.value);
+        console.log(userInput);
         $('#meaningsDiv').html("");
         $('#examplesDiv').html("");
         fetchData(userInput);
+        fetchSwedishDefinition(userInput);
     }
 })
 $('#search-button').on('click', (e)=>{
     let userInput = $('#searchField').val();
+    userInput = trimUserinput(userInput);
     $('#meaningsDiv').html("");
     $('#examplesDiv').html("");
     fetchData(userInput);
+    fetchSwedishDefinition(userInput);
 })
 
-
+function trimUserinput (input){
+    newInput = input.toLowerCase().trim().replace(/\s\s+/g, ' ');
+    return newInput;
+}
 function fetchData (userInput){
 let request = $.ajax();
  //test if the string matches a regex, if arabic or latin to call the proper ajax
@@ -35,9 +43,12 @@ if (/[\u0600-\u06FF]+/g.test(userInput)){
 }
           
 request.then(data => {
+    console.log(data);
     let meanings = data.tuc;
     let examples = data.examples;
     let mapedMeanings = meanings.filter(item => item.phrase != null).map(item=> item.phrase.text);
+    console.log(mapedMeanings);
+    console.log(examples);
     mapedMeanings.forEach(word => {
         $('#meaningsDiv').append(createMeanings(word));
     })
@@ -58,4 +69,27 @@ function createExamples (example){
             </ul>`
 }
 
-});
+function fetchSwedishDefinition (userInput){
+    // request swedish definitions only if the user input is in swedish
+    if (/[\u0000-\u007F]+/g.test(userInput)){
+        $.ajax({
+            method: 'GET',
+            url: "https://glosbe.com/gapi/translate?from=swe&dest=swe&format=json&phrase=" + userInput + "&pretty=true&tm=true",
+            dataType: 'jsonp'
+        }).then(data=>{
+        let meanings = data.tuc;
+        let mapedMeanings = meanings.filter(item => item.phrase != null).map(item=> item.phrase.text);
+        console.log(mapedMeanings);
+        mapedMeanings.forEach(word => {
+            $('#meaningsDiv').append(createSwedishMeaning(word));
+        })
+        })
+    }
+    
+}
+function createSwedishMeaning(word){
+    return `<li class="list-group-item bg-success">${word}</li>`
+}
+
+}); //jQuery ready
+
